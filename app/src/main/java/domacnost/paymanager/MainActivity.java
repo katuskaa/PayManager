2294ad934af2c6d1bf1a7c39d8e2c8e44838c6eb
hlpeter <phlatky8@gmail.com>
2017-07-23 13:34:06 +0200

    new activity

--- app/src/main/java/domacnost/paymanager/MainActivity.java
+++ app/src/main/java/domacnost/paymanager/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.welcome_activity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
